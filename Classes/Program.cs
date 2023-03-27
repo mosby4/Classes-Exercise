4f1f7bb0361bd1c1caf45ae76158fffd6dce7c37
Mosby4 <rufus.hamby@gmial.com>
2023-03-27 09:52:47 -0500

    Classes completed

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var MyCar = new Cars();
+            MyCar.Make = "Aerial Atom";
+            MyCar.Model = "Atom Four";
+            MyCar.Year = 2023;
+            Console.WriteLine($"My new race car is an {MyCar.Make}.");
+            Console.WriteLine($"More specifcly a {MyCar.Year }  {MyCar.Model}.");
         }
     }
 }
